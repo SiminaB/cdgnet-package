648eb6a7a071c8b0006dce0282c1bf0dfbb59b62
hcorrada <hcorrada@gmail.com>
2020-05-22 13:05:42 -0400

    check and load in app function

--- R/cdgnet.R
+++ R/cdgnet.R
@@ -4,6 +4,8 @@
 #'@export
 #'
 cdgnetApp <- function(database=NULL, names=NULL) {
+  .check_and_load_KEGG()
+
   # Run the application
   shinyjs::useShinyjs()
   shinyApp(ui = .cdgnetUI, server = .cdgnetServer)
