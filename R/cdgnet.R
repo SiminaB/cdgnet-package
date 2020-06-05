a7125a6e889e943cb17ed9c8b8245e4d3dc1c403
hcorrada <hcorrada@gmail.com>
2020-06-05 10:10:25 -0400

    deal with kegg data in a different way

--- R/cdgnet.R
+++ R/cdgnet.R
@@ -3,12 +3,24 @@
 #'
 #'@export
 #'
-cdgnetApp <- function(database=NULL, names=NULL) {
+cdgnetApp <- function(list_paths_KEGG_file, database=NULL, names=NULL) {
   # Run the application
   shinyjs::useShinyjs()
 
+  data(DrugBank_targets)
+  data(drugs_PO_FDA_biomarkers)
+  data(drugs_PO_FDA_targets)
+  data(KEGG_cancer_paths_onc_long)
+  data(FDA_approved_drugs)
+  data(non_prop2prop)
+  data(prop2non_prop)
+  data(prop_non_prop)
+  data(Onc_df)
+
+  load(list_paths_KEGG_file)
   cat("list paths within app function")
   print(names(list_paths_KEGG))
+
   shinyApp(ui = .cdgnetUI, server = .cdgnetServer)
 }
 
@@ -19,10 +31,12 @@ cdgnetApp <- function(database=NULL, names=NULL) {
 #'
 #' @export
 runCDGnet <- function() {
-  this_dir <- getwd()
-  on.exit(setwd(this_dir))
+  list_paths_KEGG_file <- file.path(system.file("appdir", package="CDGnet"), "list_paths_KEGG.RData")
 
-  setwd(file.path(system.file("appdir", package="CDGnet")))
-  source("app.R")
-  shiny::runApp(cdgnetApp())
+  if (!file.exists(list_paths_KEGG_file)) {
+    stop("KEGG data not found. Download with function CDGnet::download_and_process_KEGG()")
+  }
+
+
+  shiny::runApp(cdgnetApp(list_paths_KEGG_file))
 }
--- inst/appdir/app.R
+++ inst/appdir/app.R
@@ -3,23 +3,10 @@ options(repos = BiocManager::repositories())
 
 library(CDGnet)
 
-if (!file.exists("list_paths_KEGG.RData")) {
+list_paths_KEGG_file <- file.path(getwd(), "list_paths_KEGG.RData")
+
+if (!file.exists(list_paths_KEGG_file)) {
   stop("KEGG data not found. Download with function CDGnet::download_and_process_KEGG()")
 }
 
-load("list_paths_KEGG.RData")
-
-cat("list_paths_KEGG names:")
-print(names(list_paths_KEGG))
-
-data(DrugBank_targets)
-data(drugs_PO_FDA_biomarkers)
-data(drugs_PO_FDA_targets)
-data(KEGG_cancer_paths_onc_long)
-data(FDA_approved_drugs)
-data(non_prop2prop)
-data(prop2non_prop)
-data(prop_non_prop)
-data(Onc_df)
-
-CDGnet::cdgnetApp()
+CDGnet::cdgnetApp(list_paths_KEGG_file)
